dd5912e6779113fe0c71f245d531122447315366
Ashot Danielyan <37475615+dreconater@users.noreply.github.com>
2024-06-21 15:27:27 +0400

    Working on Main Menu

--- Wind of Change/Assets/Scripts/MainMenuEngine.cs
+++ Wind of Change/Assets/Scripts/MainMenuEngine.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuEngine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Menu Part")]
+    public Animator Buttons;
+    
+    public Button PlayBtn;
+    public Button MusicOnOFFButton;
+    public TMP_Text MusicBtnText;
+    public Button QuitButton;
+
+    public AudioSource BgAudio;
+
+    private void Awake()
     {
-        
+        MusicOnOFFButton.onClick.AddListener(MusicOnOff);
+        QuitButton.onClick.AddListener(QuitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        int bgMusicValue = PlayerPrefs.GetInt("BgMusic");
+
+        if (bgMusicValue == 0)
+        {
+            BgAudio.Pause();
+            MusicBtnText.text = "Music - Off";
+        }
+        else
+        {
+            BgAudio.Play();
+            MusicBtnText.text = "Music - On";
+        }
+    }
+
+    void MusicOnOff() { 
+        if (BgAudio.isPlaying) {
+            BgAudio.Pause();
+            MusicBtnText.text = "Music - Off";
+            PlayerPrefs.SetInt("BgMusic", 0);
+        }
+        else
+        {
+            BgAudio.Play();
+            MusicBtnText.text = "Music - On";
+            PlayerPrefs.SetInt("BgMusic", 1);
+        }
+    }
+
+    void QuitGame() {
+        // this will not work inside editor
+
+        Application.Quit();
     }
 }
